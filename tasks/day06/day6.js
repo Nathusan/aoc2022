8ab91b0b1e68e646122bf8e76e83fa544affd669
Nathusan <44724888+Nathusan@users.noreply.github.com>
2022-12-11 13:45:38 +0000

    initial

---
+++ tasks/day06/day6.js
@@ -0,0 +1,43 @@
+import { readFileSync } from 'node:fs'
+
+const input = readFileSync("tasks/day06/day06.txt", { encoding: 'utf-8'})
+    .replace(/\r/g, '') //remove all \r characters
+    .trim(); //remove whitespace
+
+function isUnique(array) {
+    return new Set(array).size === array.length;
+}
+
+function part1() {
+    let slidingWindow = []
+    for( let i = 0; i < input.length; i++) {
+        slidingWindow.push(input[i]);
+
+        if (slidingWindow.length > 4) {
+            slidingWindow.shift();
+        }
+
+        if (slidingWindow.length === 4 && isUnique(slidingWindow)){
+            console.log(i+1)
+            break;
+        }
+    }
+}
+
+function part2() {
+    let slidingWindow = []
+    for( let i = 0; i < input.length; i++) {
+        slidingWindow.push(input[i]);
+
+        if (slidingWindow.length > 14) {
+            slidingWindow.shift();
+        }
+
+        if (slidingWindow.length === 14 && isUnique(slidingWindow)){
+            console.log(i+1)
+            break;
+        }
+    }
+}
+
+part1()
---
+++ tasks/template copy/template.js
@@ -0,0 +1,50 @@
+import { readFileSync } from 'node:fs'
+
+const input = readFileSync("tasks/template/input.txt", { encoding: 'utf-8'})
+    .replace(/\r/g, '') //remove all \r characters
+    .trim().split(/\n?\n\n/); //remove whitespace
+
+
+function arraySum(arrNum) {
+    return arrNum.reduce((partialSum, a) => Number(partialSum) + Number(a), 0);
+}
+
+function part1() {
+    let biggestElf = 0;
+    const recordOfAll = [];
+    const arrayOfElf = [];
+
+    for (let i = 0; i < input.length;i++) {
+        const array = input[i].replace(/\n/g, " ").split(" ");
+        arrayOfElf.push(array)
+    }
+
+
+    for(let x = 0; x < arrayOfElf.length;x++) {
+        let sum = 0;
+        if (arrayOfElf[x].length > 1 ) {
+            sum = arraySum(arrayOfElf[x])
+        } else {
+            sum = Number(arrayOfElf[x]);
+        }
+
+        if (sum > biggestElf) {
+            biggestElf = sum
+            console.log(sum)
+        }
+        recordOfAll.push(sum)
+
+    }
+
+    console.log(biggestElf)
+    console.log(recordOfAll)
+    const sorted = recordOfAll.sort((a,b) => b-a);
+    console.log(arraySum(sorted.slice(0,3)))
+}
+
+
+function part2() {
+}
+
+part1()
+part2()
